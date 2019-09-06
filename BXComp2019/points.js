63f77b3704239be0f0bcc1e52196ac6ec141b87a
João Francisco Cocca Fukuda <joao.fukuda@usp.br>
2019-09-05 23:03:28 -0300

    Some velocity changes Retarded the velocity of the ships, for now...

--- BXComp2019/points.js
+++ BXComp2019/points.js
@@ -1,8 +1,8 @@
 //calling the function in window.onload to make sure the HTML is loaded
 window.onload = function() {
-    //our box element
-    var ships = document.getElementsByClassName('ship');
-    var t = setInterval(delay, 500);
+	//our box element
+	var ships = document.getElementsByClassName('ship');
+	var t = setInterval(delay, 500);
 
 	var height = 50;
 	var pos = [0, 0, 0, 0];
@@ -19,18 +19,20 @@ window.onload = function() {
 		t = setInterval(move, 10);
 	}
 
-    function move() {
+	function move() {
 		var endedall4 = 0;
 		for(var n = 0; n < ships.length; n++)
 		{
 			ships[n].style.left = pos[n]+'px';
-	        if(pos[n] >= end[n]) {
+			if(pos[n] >= end[n])
+			{
 				endedall4 += 1;
-	        }
+			}
 			var deltapos = Math.pow((pos[n]/end[n])-0.5,2);
 			if(deltapos < 1/17) deltapos = 1/17;
-			pos[n] += end[n]/(deltapos*700);
+			pos[n] += end[n]/(deltapos*1000);
 		}
 		if(endedall4 == 4) clearInterval(t);
-    }
+	}
 };
+
