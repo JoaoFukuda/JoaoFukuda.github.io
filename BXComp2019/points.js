8acc1ba642040b0b95ace89f6c575852666a7955
João Francisco Cocca Fukuda <joao.fukuda@usp.br>
2019-09-05 23:38:30 -0300

    Added images to the play

--- BXComp2019/points.js
+++ BXComp2019/points.js
@@ -4,13 +4,19 @@ window.onload = function() {
 	var ships = document.getElementsByClassName('ship');
 	var t = setInterval(delay, 500);
 
-	var height = 50;
+	var height = 0;
 	var pos = [0, 0, 0, 0];
 	var end = [500, 350, 200, 150];
 
 	for(var n = 0; n < ships.length; n++)
 	{
 		ships[n].style.top = (n * height)+"px";
+		ships[n].innerHTML="<img src='trail.png' /><img src='ship.png' />";
+		ships[n].children[1].style.height = '100%';
+		ships[n].children[1].style.width = 'auto';
+		ships[n].children[0].style.top = '15px';
+		ships[n].children[0].style.height = '50%';
+		ships[n].children[0].style.width = '15px';
 	}
 
 	function delay()
@@ -23,11 +29,13 @@ window.onload = function() {
 		var endedall4 = 0;
 		for(var n = 0; n < ships.length; n++)
 		{
-			ships[n].style.left = pos[n]+'px';
 			if(pos[n] >= end[n])
 			{
+				pos[n] = end[n];
 				endedall4 += 1;
 			}
+			ships[n].children[1].style.left = pos[n]+'px';
+			ships[n].children[0].style.width = (pos[n] + 15)+'px';
 			var deltapos = Math.pow((pos[n]/end[n])-0.5,2);
 			if(deltapos < 1/17) deltapos = 1/17;
 			pos[n] += end[n]/(deltapos*1000);
